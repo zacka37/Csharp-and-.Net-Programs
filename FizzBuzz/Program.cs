3ef476260858fe045fba33afa0eefd98f8cad467
Zack Augustine <62813935+zacka37@users.noreply.github.com>
2021-12-13 01:41:05 -0600

    Add files via upload

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number < 101; number++)
+            {
+                if (number % 3 == 0 & number % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+                
+                
+            }
+            
+        }
+    }
+}
