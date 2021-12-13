b8472ccd7d0106d1d06d04a2d58c427ab6655d63
Zack Augustine <62813935+zacka37@users.noreply.github.com>
2021-12-12 20:39:03 -0600

    Add files via upload

---
+++ IT 2040/Document (Creates a .txt file)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+namespace Document
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Document\n");
+           writeDocument();
+
+        }
+        static void writeDocument() {
+            try 
+            {
+                Console.WriteLine ("Enter the name of the document");
+                string name = Console.ReadLine();
+                StreamWriter file = File.AppendText($"{name}.txt");
+                string fileName = (name + ".txt");
+                Console.WriteLine("Enter the content of the document");
+                string fileContent = Console.ReadLine();
+                file.WriteLine(fileContent);
+                file.Close();
+
+                // read
+                StreamReader fileReader = new StreamReader(fileName);
+                Console.WriteLine($"{fileName} was successfully saved. The document contains {fileContent.Length} characters.");
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine($"Exception: {err.Message}");
+            }
+            // read
+            
+        }
+    }
+}
